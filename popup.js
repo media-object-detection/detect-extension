18bd6c991c8810a482c9b855454b43a96f214d2a
Dabin Jeong <allempti@gmail.com>
2023-09-30 05:19:12 +0900

    Feature Capture
    - vision ai가 물체를 감지하면 새로운 팝업창에 감지한 물체 버튼 생성
    - 버튼을 클릭하면 해당 물체의 이미지만 저장하도록 변경

--- popup.js
+++ popup.js
@@ -2,82 +2,87 @@ document.addEventListener('DOMContentLoaded', function() {
   const captureButton = document.getElementById('capture');
   
   captureButton.addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.windows.create({
+      url: 'popup2.html',
+      type: 'popup',
+      width: 400,
+      height: 300,
+    });
+    
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.scripting.executeScript({
-        target: {tabId: tabs[0].id},
-        func: captureScreen
+        target: { tabId: tabs[0].id },
+        func: captureScreen,
       });
     });
   });
-  
+
+  // 유튜브 영상을 캡처하고 vision ai를 통해 객체를 감지
   function captureScreen() {
+
     const video = document.querySelector('video');
     if (!video) {
-      console.error('No video element found');
+      alert('No video element found');
       return;
     }
-  
+
     const canvas = document.createElement('canvas');
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
     const context = canvas.getContext('2d');
     context.drawImage(video, 0, 0, canvas.width, canvas.height);
-  
-    const dataURL = canvas.toDataURL('image/png');
-  
-    // Base64 인코딩된 이미지 데이터 전송
-    fetch(`https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        requests: [
-          {
-            image: {
-              content: dataURL.split(',')[1]
-            },
-            features: [
+
+    const dataURL = canvas.toDataURL('image/png').split(',')[1];
+
+    // background.js에 API 키를 요청
+    chrome.runtime.sendMessage({ action: "getApiKey" }, function(response) {
+      if (response) {
+        const apiKey = response.API_KEY;
+
+        fetch(`https://vision.googleapis.com/v1/images:annotate?key=${apiKey}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            requests: [
               {
-                type: 'OBJECT_LOCALIZATION',
-                maxResults: 5
-              }
-            ]
+                image: {
+                  content: dataURL,
+                },
+                features: [
+                  {
+                    type: 'OBJECT_LOCALIZATION',
+                    maxResults: 5,
+                  },
+                ],
+              },
+            ],
+          }),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+          if (
+            data.responses &&
+            data.responses[0] &&
+            data.responses[0].localizedObjectAnnotations
+          ) {
+            const detectList = data.responses[0].localizedObjectAnnotations;
+
+            // popup2.html로 데이터를 전송
+            chrome.runtime.sendMessage({ data: { detectList, dataURL } }, function(response) {
+                console.log('popup1.js: success');
+            });
+
           }
-        ]
-      }),
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Vision API 응답:', data);
-      if (data.responses && data.responses[0] && data.responses[0].localizedObjectAnnotations) {
-        const annotations = data.responses[0].localizedObjectAnnotations;
-        annotations.forEach((object, index) => {
-          const vertices = object.boundingPoly.normalizedVertices;
-          const startX = vertices[0].x * canvas.width;
-          const startY = vertices[0].y * canvas.height;
-          const endX = vertices[2].x * canvas.width;
-          const endY = vertices[2].y * canvas.height;
-  
-          const croppedCanvas = document.createElement('canvas');
-          croppedCanvas.width = endX - startX;
-          croppedCanvas.height = endY - startY;
-          const croppedContext = croppedCanvas.getContext('2d');
-          croppedContext.drawImage(canvas, startX, startY, croppedCanvas.width, croppedCanvas.height, 0, 0, croppedCanvas.width, croppedCanvas.height);
-  
-          const croppedDataURL = croppedCanvas.toDataURL('image/png');
-  
-          const a = document.createElement('a');
-          a.href = croppedDataURL;
-          a.download = `object_${index}.png`;
-          document.body.appendChild(a);
-          a.click();
-          document.body.removeChild(a);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
         });
+
+      } else {
+        console.error('API 키를 찾을 수 없음.');
       }
-    })
-    .catch((error) => {
-      console.error('Error:', error);
     });
   }
 });
